b53eed0d91cf753dfe31d693a259d4ed834dfefa
jackwtp <marialuiza7655@gmail.com>
2023-12-06 09:12:28 -0300

    k

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Nsei from './sla/sla';
+import './App.css';
+
+function App() {
+  return <Nsei name="pedrinho 100%"></Nsei>;
+}
+
+export default App;
---
+++ src/sla/sla.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+function Nsei (props) {
+    return (<h1> Bem-vindo, {props.name}</h1>)
+}
+export default Nsei;
